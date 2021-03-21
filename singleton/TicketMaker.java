365f66866e0b7cbbdc6243f5469a3f35f4250780
ikam <els0212@nate.com>
2021-03-21 23:31:26 +0900

    singleton pattern

---
+++ singleton/TicketMaker.java
@@ -0,0 +1,15 @@
+package singleton;
+
+public class TicketMaker {
+	private static TicketMaker ticketMaker = new TicketMaker();
+	private static int ticket = 1000;
+	private TicketMaker() {
+		
+	}
+	public static TicketMaker getInstance() {
+		return ticketMaker;
+	}
+	public int getNextTicketNumber() {
+		return ticket++;
+	}
+}
